e73cb46d5a1f0d666d15ff28f26fa57ca9b9e4b4
HarveyJPA <Harvey.J.ParkerAcheson@aexp.com>
2021-09-17 13:32:53 +0100

    added .idea, EmailExtract.iml and sample.txt files

--- EmailExtract/src/EECore.java
+++ EmailExtract/src/EECore.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class EECore {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.regex.*;
+
+public class EECore {
+    public static void main(String[] args) {
+        filePrinter();
+    }
+
+    private static void filePrinter() {
+        try {
+            FileReader reader = new FileReader("/Users/hparkera/ghstuff/email-extraction/sample.txt");
+            BufferedReader bufferedReader = new BufferedReader(reader);
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                int count = 0;
+                System.out.println(line);
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
+
